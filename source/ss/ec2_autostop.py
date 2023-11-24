f604ed17982f4f631cfceedaf2b20d3c01ad5ade
J  Manasa <64403837+jami-manasa@users.noreply.github.com>
2023-11-24 14:09:00 -0600

    Update ec2_autostop.py

--- source/ss/ec2_autostop.py
+++ source/ss/ec2_autostop.py
@@ -143,10 +143,10 @@ def non_statefull_auto_stop(non_statefull):
 wanted_columns=["account_id","region","instance_id","instance_tag_name","account_name","instance_state","auto_stop_enable","recent_launch_time" ]
 print("hhihihi manasa dont worry")
 try:
-    print("om...............................................>")
+    # print("om...............................................>")
     data_from_database=get_dbdata_with_columns("ss.ec2_instances_schedules",wanted_columns) 
 except Exception as e:
-        print(e,"hihihihhihihihh")
+        # print(e,"hihihihhihihihh")
 print(data_from_database)
 # print("--------------------------------------------------------------------------.>")
 # if data_from_database:
