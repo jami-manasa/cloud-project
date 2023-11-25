1d3fb5dee7de21f63a89b9394f678e3738638dd5
J  Manasa <64403837+jami-manasa@users.noreply.github.com>
2023-11-24 20:24:16 -0600

    Update ec2_autostop.py

--- source/ss/ec2_autostop.py
+++ source/ss/ec2_autostop.py
@@ -23,25 +23,7 @@ def stop_instances(ec2,id,connection):
     except ClientError as e:
         print('Error', e)
 
-       
-# def get_dbdata_with_columns(database_with_schema,wanted_columns):
-    
-#     col = ','.join([str(elem) for elem in wanted_columns])
-#     try:
-#         database=database_with_schema.split('.')
-#         cursor = connection.cursor()
-#         query = " select {0} FROM {1} WHERE auto_stop_enable='{2}' AND instance_state= '{3}' ;".format(col,database[1],str('true'),str('running')) 
-#         cursor.execute(query)
-#         data_from_database = pd.DataFrame(cursor.fetchall(),columns=wanted_columns)
-#         if data_from_database.empty:
-#             print('No instance is ready for auto auto stop')
-#         else:
-#             print("Some intances  are ready for auto auto stop.")
-#         connection.commit()
-#         return data_from_database
-#     except:
-#         print("check whether the database exits the exact columns") 
-
+        
 
 
 def get_dbdata_with_columns(database_with_schema,wanted_columns):
@@ -127,44 +109,7 @@ def statefull_auto_stop(statefull):
         print("no need of autosotp for this  statefull set ")
 
        
-def non_statefull_auto_stop(non_statefull):
-    records=non_statefull
-    try:
-        for i in range(len(records)):
-            acc_id=list(records['account_id'])[i]
-            acc_name=list(records['account_name'])[i]
-            region=list(records['region'])[i]
-            reg_keys=get_awsaccount_details(acc_id,connection)
-            assume_role=reg_keys['assume_role'][0]
-            launch_time=list(records['recent_launch_time'])[i]
-            k=launch_time.replace("+00:00","")
-            date_time_obj = dt.datetime.strptime(k, '%Y-%m-%d %H:%M:%S')
-            diff= (datetime.utcnow() - timedelta(minutes=10))-date_time_obj
-            sec = diff.total_seconds()
-            ins_id=list(records["instance_id"])[i]
-            cloudwatch_client = create_client(acc_id,region,assume_role,'cloudwatch')
-            start_time = datetime.utcnow() - timedelta(hours=2,minutes=5)
-            endTime = datetime.utcnow() - timedelta(minutes=5)
-            response = cloudwatch_client.get_metric_statistics(Namespace='AWS/EC2',MetricName='CPUUtilization',Period=7200,
-                                                        Dimensions=[{
-                                                            'Name': 'InstanceId','Value':ins_id},],
-                                                        StartTime=start_time,
-                                                        EndTime=endTime,
-                                                        Statistics=['Maximum'])                                      
-            maximum  = jmespath.search("Datapoints[*].[Maximum]",response)
-            # print(max(maximum)[0],"---",ins_id,sec)
-            if sec > 7200:
-                if (max(maximum)[0]) < 6: 
-                    ec2 = create_client(acc_id,region,assume_role, 'ec2')
-                    print("Needs To Stop In account, region:",acc_name, region,ins_id)
-                    stop_instances(ec2,ins_id,connection)
-                else:
-                    print("No Need To autoStop ,bcz cpu utilization is more than 6.") 
-            else:
-                print("Instance started recently, so no need to auto stop.")
-            
-    except Exception as e:
-        print(e)
+
     
 wanted_columns=["account_id","region","instance_id","instance_tag_name","account_name","instance_state","auto_stop_enable","recent_launch_time" ]
 # print(wanted_columns)
@@ -176,7 +121,7 @@ if data_from_database.empty:
         
 else:
     non_statefull =data_from_database[data_from_database.auto_stop_enable == 'true']
-    t2=Thread(target = non_statefull_auto_stop(non_statefull))
+    t2=Thread(target = statefull_auto_stop(non_statefull))
     t2.start() 
    
     
