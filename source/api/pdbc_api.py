007e9e0797b8bcd47503861f9e3ed05ee07e56ae
J  Manasa <64403837+jami-manasa@users.noreply.github.com>
2023-11-24 20:06:11 -0600

    Update pdbc_api.py

--- source/api/pdbc_api.py
+++ source/api/pdbc_api.py
@@ -215,7 +215,7 @@ def get_dbdata_with_columns(database_with_schema,wanted_columns):
         connection.commit()
         return data_from_database
     except:
-        print("check whether the database exits the exact columns---------------------------------------------------------------------------------> jai shree ram")
+        print("check whether the database exits the exact column")
 
 def remove_records(database_with_schema,update_by,update_value):
     database=database_with_schema.split('.')
