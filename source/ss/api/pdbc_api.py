8e6c541491ecf68dc3f6d7e75080f92fab2b7d18
J  Manasa <64403837+jami-manasa@users.noreply.github.com>
2023-11-28 13:07:19 -0600

    Update pdbc_api.py

--- source/ss/api/pdbc_api.py
+++ source/ss/api/pdbc_api.py
@@ -136,6 +136,9 @@ def data_crud_operation(connection,count,data_from_cloud,updating_by,database_wi
                     data_from_cloud.drop(ignore_columns, axis='columns', inplace=True)
                     ignore_columns.append(pk)
                     print(ignore_columns,"----------------------------------------------------------9999999999999999")
+                    print(data_from_cloud)
+                    print("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------..........>>>>>>>>>>>>>>>>")
+                    print(data_from_database)
                     data_from_cloud = pd.merge(data_from_cloud, data_from_database[ignore_columns], how='left', on=pk)
                     ignore_columns.pop()
                 except Exception as e:
